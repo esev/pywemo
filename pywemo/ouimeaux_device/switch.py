1b9c1bf56cda84fb6bf6b64d6921733342e77ab1
Eric Severance <esev@esev.com>
2021-12-21 22:12:03 -0800

    Type checking for pywemo/ouimeaux_device/switch.py

--- pywemo/ouimeaux_device/switch.py
+++ pywemo/ouimeaux_device/switch.py
@@ -1,4 +1,6 @@
 """Representation of a WeMo Switch device."""
+from __future__ import annotations
+
 from . import Device
 from .api.service import RequiredService
 
@@ -7,24 +9,24 @@ class Switch(Device):
     """Representation of a WeMo Switch device."""
 
     @property
-    def _required_services(self):
+    def _required_services(self) -> list[RequiredService]:
         return super()._required_services + [
             RequiredService(name="basicevent", actions=["SetBinaryState"]),
         ]
 
-    def set_state(self, state):
+    def set_state(self, state: int) -> None:
         """Set the state of this device to on or off."""
-        self.basicevent.SetBinaryState(BinaryState=int(state))
-        self._state = int(state)
+        self.basicevent.SetBinaryState(BinaryState=state)
+        self._state = state
 
-    def off(self):
+    def off(self) -> None:
         """Turn this device off. If already off, will return "Error"."""
-        return self.set_state(0)
+        self.set_state(0)
 
-    def on(self):
+    def on(self) -> None:
         """Turn this device on. If already on, will return "Error"."""
-        return self.set_state(1)
+        self.set_state(1)
 
-    def toggle(self):
+    def toggle(self) -> None:
         """Toggle the switch's state."""
-        return self.set_state(not self.get_state())
+        self.set_state(not self.get_state())
